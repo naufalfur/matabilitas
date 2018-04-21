e1313cd8f352a0b824a4155ac0126c327d7d4e9f
Muhammad Naufal <Muhnaufalfurqon77@gmail.com>
2018-04-21 21:40:20 +0700

    Procfile, AppServiceProviders

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        if(env('REDIRECT_HTTPS')) {
+            URL::forceScheme('https'); //for heroku deployment
+        }
     }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
-        //
+        if(env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true); //for heroku deployment
+        }
     }
 }
